48e9151eef4a44fb75d84567079fbd7bec236e87
thor2643 <Thor951753@hotmail.com>
2023-09-26 09:46:16 +0200

    First Setup

---
+++ ImageAnalyzer.py
@@ -0,0 +1,14 @@
+import cv2
+
+
+class ImageAnalyzer:
+    def __init__(self) -> None:
+        #[[x,y], type (1,2,3..), crowns]
+        self.classification_list = [[]]
+        
+
+    def count_crowns(self, tile) -> int:
+        pass
+
+    def calculate_score(self, classification_list):
+        pass
